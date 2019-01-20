18f125cad1d2c918ca17381a65529373d24ac67c
Zuku271 <adrian.zuchowski@outlook.com>
2019-01-20 19:32:06 +0100

    magic stone 2

--- magicstones.cpp
+++ magicstones.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
-#include <array>
+#include <vector>
 
 struct Stone
 {
     unsigned int stoneX;
     unsigned int stoneY;
 
-    Stone(unsigned int X = 0, unsigned int Y = 0) : stoneX(X), stoneY(Y) {};
+    Stone(unsigned int X, unsigned int Y) : stoneX(X), stoneY(Y) {};
+    Stone(const Stone& s) {stoneX = s.stoneX; stoneY = s.stoneY;};
+    Stone(const Stone&&) = delete;
 };
 
 bool operator==(const Stone& stone1, const Stone& stone2)
@@ -32,11 +34,11 @@ bool isInside(const Stone& stone1, const Stone& stone2, const Stone& stone3)
 
     if (!dX)
     {
-        const int sX1 = stone1.stoneX + dY;
-        const int sY1 = stone1.stoneY;
+        const unsigned int sX1 = stone1.stoneX + dY;
+        const unsigned intsY1 = stone1.stoneY;
 
-        const int sX2 = stone2.stoneX + dY; // same Y as stone 1
-        const int sY2 = stone2.stoneY;
+        const unsigned int sX2 = stone2.stoneX + dY; // same Y as stone 1
+        const unsigned int sY2 = stone2.stoneY;
 
         if (stone3.stoneX >= stone1.stoneX && stone3.stoneX <= sX1 && stone3.stoneY >= stone1.stoneY && stone3.stoneY <= sY2)
             return true;
@@ -45,11 +47,11 @@ bool isInside(const Stone& stone1, const Stone& stone2, const Stone& stone3)
     }
     else if (!dY)
     {
-        const int sX1 = stone1.stoneX;
-        const int sY1 = stone1.stoneY + dX;
+        const unsigned int sX1 = stone1.stoneX;
+        const unsigned int sY1 = stone1.stoneY + dX;
 
-        const int sX2 = stone2.stoneX; // same X as stone 1
-        const int sY2 = stone2.stoneY + dX;
+        const unsigned int sX2 = stone2.stoneX; // same X as stone 1
+        const unsigned int sY2 = stone2.stoneY + dX;
 
         if (stone3.stoneX >= stone1.stoneX && stone3.stoneX <= stone2.stoneX && stone3.stoneY >= stone1.stoneY && stone3.stoneY <= sY2)
             return true;
@@ -82,12 +84,40 @@ bool isSquare(const Stone& stone1, const Stone& stone2)
 
     if (dX == 0 || dY == 0)
         return true;
-    else if (!(dX - dY))
-        return false;
+
+    if (dX == dY)
+        return true;
+
+    return false;
+}
+
+int silnia(int n)
+{
+    if (n == 0)
+        return 1;
+    if (n == 1)
+        return 1;
+    if (n > 1)
+        return n * silnia(n - 1);
 }
 
 int main()
 {
+    unsigned int N;
+    std::cin >> N;
+    std::vector<Stone> stoneVect;
+
+    for (int i = 0; i < N; ++i)
+    {
+        Stone s(0, 0);
+        std::cin >> s.stoneX >> s.stoneY;
+        stoneVect.push_back(s);
+    }
+
+    for (unsigned int i = 0; i < silnia(N - 1); ++i)
+    {
+
+    }
     
     return 0;
 }
